debebdeda4508fe55190a5478bed49f7f7632baa
Jonathan Arnett <jonarnett90@gmail.com>
2014-12-25 14:09:45 -0500

    Added titles to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022143652) do
+ActiveRecord::Schema.define(version: 20141225183922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141022143652) do
   create_table "projects", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "tutorials", force: true do |t|
