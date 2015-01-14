e52f21c653b7d4f3780b0892545c7297ed9cf5b7
Jonathan Arnett <jonarnett90@gmail.com>
2015-01-14 17:44:46 -0500

    Approved projects in Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110075620) do
+ActiveRecord::Schema.define(version: 20150114223423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150110075620) do
     t.string   "title"
     t.integer  "leader_id"
     t.text     "description"
+    t.boolean  "approved",    default: false
   end
 
   create_table "projects_users", id: false, force: :cascade do |t|
