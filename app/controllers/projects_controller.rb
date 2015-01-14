e38cace17571d5581f9c740b1a9480351904d575
Jonathan Arnett <jonarnett90@gmail.com>
2015-01-14 17:46:10 -0500

    Only list approved projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = Project.where(approved: true)
   end
 
   def new
