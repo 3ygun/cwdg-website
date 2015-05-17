5bbcd5fc2273459102e2324230b10a3a63480f43
Jonathan Arnett <jonarnett90@gmail.com>
2015-05-17 17:27:02 -0400

    Semi-working test cases
    The "syncing adds post" does not work because the Twitter gem is not
    accepting my fake Twitter response. Twitter replies with JSON, so I
    have a suitable (I believe) response being passed back in the form of a
    string. I tried doing a hash (like for the GitHub mock), but Webmock
    does not accept hashes (Arrays are fine, though). This throws a syntax
    error.
    
    In other news, "syncing updates posts" works fine.

--- test/jobs/sync_posts_job_test.rb
+++ test/jobs/sync_posts_job_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class SyncPostsJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # GitHub mocks
+    body = [{
+      :name => "git-tutorial.md",
+      :path => "git-tutorial.md",
+      :sha => "744a01b55be2a912a821ab6c070460723e533aa2",
+      :size => 7341,
+      :url => "https://api.github.com/repos/CWDG/blog/contents/git-tutorial.md?ref=master",
+      :html_url => "https://github.com/CWDG/blog/blob/master/git-tutorial.md",
+      :git_url => "https://api.github.com/repos/CWDG/blog/git/blobs/744a01b55be2a912a821ab6c070460723e533aa2",
+      :download_url => "https://raw.githubusercontent.com/CWDG/blog/master/git-tutorial.md",
+      :type => "file",
+      :_links => {
+        :self => "https://api.github.com/repos/CWDG/blog/contents/git-tutorial.md?ref=master",
+        :git => "https://api.github.com/repos/CWDG/blog/git/blobs/744a01b55be2a912a821ab6c070460723e533aa2",
+        :html => "https://github.com/CWDG/blog/blob/master/git-tutorial.md"
+      }
+    }]
+    stub_request(:get, "https://api.github.com/repos/CWDG/blog/contents/").
+      to_return(:status => 200, :body => body, :headers => {})
+    stub_request(:get, 'https://raw.githubusercontent.com/CWDG/blog/master/git-tutorial.md').
+      to_return(status: 200, body: "# Git Tutorial")
+
+    # Twitter mocks
+    stub_request(:post, "https://api.twitter.com/1.1/statuses/update.json").
+      to_return(status: 200, body: "{'id': 1234567890}")
+  end
+
+  test "syncing adds post" do
+    SyncPostsJob.perform_now
+
+    assert_equal 1, Tutorial.count
+    assert_equal "Git Tutorial", Tutorial.first.title
+  end
+
+  test "syncing updates posts" do
+    Tutorial.create!({file_name: "git-tutorial.md", title: "Git Tutorial",
+                      sha: "1234", content: "# Old Content"})
+
+    SyncPostsJob.perform_now
+
+    assert_equal "Git Tutorial", Tutorial.first.title
+  end
 end
